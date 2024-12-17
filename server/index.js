5aaeecf2e5179e6f62e0c29edd14db666311b2f5
Shiva Kansara <63890928+Shivaaykansara@users.noreply.github.com>
2024-12-17 16:51:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 const corsOption = {
-  origin:'http://localhost:5173',
+  origin:'https://interactivequiz-bur6.onrender.com',
   methods:'GET, PUT, POST, PATCH, DELETE, HEAD',
   credentials:true
 }
