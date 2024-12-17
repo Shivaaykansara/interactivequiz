2b5987fbc9bfe5eb57c515327c118f5a31b26ecf
Shiva Kansara <63890928+Shivaaykansara@users.noreply.github.com>
2024-12-17 16:42:07 +0530

    Update categorizedQuestionService.js

--- client/src/services/categorizedQuestionService.js
+++ client/src/services/categorizedQuestionService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000';
-export const API = 'http://localhost:5000';
+const API_BASE_URL = 'https://interactivequiz-backend.onrender.com';
+export const API = 'https://interactivequiz-backend.onrender.com';
 
 
 export const categorizedQuestionService = {
